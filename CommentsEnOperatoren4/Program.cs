817fedaaca0947f3af890e47005172503add98ed
LSLIM <LSLIM@LS>
2018-02-08 21:41:52 +0100

    I add Henk(55) and Piet(22)

--- CommentsEnOperatoren4/Program.cs
+++ CommentsEnOperatoren4/Program.cs
@@ -10,6 +10,16 @@ namespace CommentsEnOperatoren4
     {
         static void Main(string[] args)
         {
+            /// Maak een console - applicatie, waarin u de leeftijd van Piet(55) en Henk(22) als variabelen opslaat.
+            /// Laat middels de tenary operator het systeem bepalen welke van de twee het oudste is.
+            /// Print daarnaast de totale leeftijd.
+            int Piet = 55;
+            int Henk = 22;
+
+            int TotaleLeeftijd = Piet + Henk;
+            Console.WriteLine(Piet > Henk ? "Piet" : "Henk");
+            Console.WriteLine(TotaleLeeftijd);
+            Console.ReadLine();
         }
     }
 }
