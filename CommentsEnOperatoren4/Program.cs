0f0bc6b8e635b325d944df2ceee2275c4746d68a
LSLIM <LSLIM@LS>
2018-02-11 08:50:51 +0100

    I add a second solution, called namespace ConsoleApplication4

--- CommentsEnOperatoren4/Program.cs
+++ CommentsEnOperatoren4/Program.cs
@@ -13,13 +13,35 @@ namespace CommentsEnOperatoren4
             /// Maak een console - applicatie, waarin u de leeftijd van Piet(55) en Henk(22) als variabelen opslaat.
             /// Laat middels de tenary operator het systeem bepalen welke van de twee het oudste is.
             /// Print daarnaast de totale leeftijd.
+            
             int Piet = 55;
             int Henk = 22;
 
             int TotaleLeeftijd = Piet + Henk;
-            Console.WriteLine(Piet > Henk ? "Piet" : "Henk");
-            Console.WriteLine(TotaleLeeftijd);
+            Console.WriteLine(Piet > Henk ? "Piet is de oudste van hun twee" : "Henk is de oudste van hun twee");
+            Console.WriteLine("De totale leeftijd van Piet en Henk is {0} ", TotaleLeeftijd);
+            ConsoleApplication4.Program2.pietHenk();
             Console.ReadLine();
+        }
+    }
+    namespace ConsoleApplication4
+    {
+        class Program2
+        {
+            public static void pietHenk()
+            {
+                int piet = 55;
+                int henk = 22;
+
+                string oudste = ((piet > henk) ? "piet" : "henk");
+                int totaal = piet + henk;
+
+                Console.WriteLine(" " + "\n" + oudste);
+                Console.WriteLine(totaal);
+                Console.ReadKey();
+                // Zorgt ervoor dat de console niet sluit. 
+                // Wacht feitelijk op een invoer van de gebruiker
+            }
         }
     }
 }
